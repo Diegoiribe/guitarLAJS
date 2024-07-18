9acfcf8de1d0f671252e83d3052ae34da289e856
Diego Iribe <diego.iribe@outlook.es>
2024-07-18 14:06:06 -0700

    creando Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,17 @@
-import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Guitar from './components/Guitar'
-import { db } from './data/db'
+import useCart from './hooks/useCart'
 
 function App() {
-  const initialCart = () => {
-    const localStorageCart = localStorage.getItem('cart')
-    return localStorageCart ? JSON.parse(localStorageCart) : []
-  }
-
-  const [data] = useState(db)
-  const [cart, setCart] = useState(initialCart)
-
-  const MIN_ITEMS = 1
-  const MAX_ITEMS = 5
-
-  useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart))
-  }, [cart])
-
-  const addToCart = (item) => {
-    const itemExists = cart.findIndex((guitar) => guitar.id === item.id)
-
-    if (itemExists >= 0) {
-      if (cart[itemExists].quantity >= MAX_ITEMS) {
-        return
-      }
-      const updatedCart = [...cart]
-      updatedCart[itemExists].quantity++
-      setCart(updatedCart)
-    } else {
-      item.quantity = 1
-      setCart([...cart, item])
-    }
-  }
-
-  const removeFromCart = (id) => {
-    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id))
-  }
-
-  const increaseQuantity = (id) => {
-    const updatedCart = cart.map((item) => {
-      if (item.id === id && item.quantity < MAX_ITEMS) {
-        return {
-          ...item,
-          quantity: item.quantity + 1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  const decreaseQuantity = (id) => {
-    const updatedCart = cart.map((item) => {
-      if (item.id === id && item.quantity > MIN_ITEMS) {
-        return {
-          ...item,
-          quantity: item.quantity - 1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  const clearCart = () => {
-    setCart([])
-  }
+  const {
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    increaseQuantity,
+    decreaseQuantity,
+    clearCart
+  } = useCart()
 
   return (
     <>
